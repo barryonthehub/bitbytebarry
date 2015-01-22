773316bfb78f1d7a084dddf8bd1e44cd2af897e5
barryonthehub <barrycohen7@gmail.com>
2015-01-22 15:57:14 +0000

    modified:   pcode/udemyPhp/quizzer/db/quizzer.sql

--- pcode/udemyPhp/quizzer/db/quizzer.sql
+++ pcode/udemyPhp/quizzer/db/quizzer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jan 16, 2015 at 06:49 PM
+-- Generation Time: Jan 22, 2015 at 04:54 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -31,7 +31,7 @@ CREATE TABLE `choices` (
   `question_number` int(11) NOT NULL,
   `is_correct` tinyint(1) NOT NULL DEFAULT '0',
   `text` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `choices`
@@ -89,7 +89,7 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `choices`
 --
 ALTER TABLE `choices`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
